a40662412c0f9041efebae5ac18b9ca432071a60
Ivan-ust <53845743+Ivan-ust@users.noreply.github.com>
2019-08-08 18:04:37 +0800

    upload Q4

---
+++ L1/Q4.py
@@ -0,0 +1,6 @@
+yr = int(input("Input the year\n"))
+if yr%4 == 0 and yr%100 != 0 :
+  print("Yes, it is a leap year")
+elif yr%400 == 0 :
+  print("Yes, it is a leap year")
+else : print("No, it is not a leap year")  
